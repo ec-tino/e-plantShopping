4d18690e7c00509a7afaee876168228af1d9c46c
ec-tino <ernestclottey756@gmail.com>
2025-10-14 22:23:15 -0400

    project complete

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -10,7 +10,7 @@ const CartItem = ({ onContinueShopping }) => {
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
     let totalAmnt = 0;
-    cart.foreach((item) => {
+    cart.forEach((item) => {
       const price = parseFloat(item.cost.replace('$', ''));
       totalAmnt += (price * item.quantity);
     })
@@ -22,9 +22,9 @@ const CartItem = ({ onContinueShopping }) => {
     onContinueShopping(e);
   };
 
-  // const handleCheckoutShopping = (e) => {
-  //   alert('Functionality to be added for future reference');
-  // };
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
     const newQuant = item.quantity + 1;
@@ -77,7 +77,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
